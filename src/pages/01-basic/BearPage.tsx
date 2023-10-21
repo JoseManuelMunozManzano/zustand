fe189f56804925b4e3f33f82737d3d8488068208
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-21 06:53:06 +0200

    1 - Bases - Refactorización

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -2,12 +2,6 @@ import { WhiteCard } from '../../components';
 import { userBearsStore } from '../../stores/bears/bears.store';
 
 export const BearPage = () => {
-  // Consumir el store.
-  // Notar que NO hacemos desestructuración.
-  // Se puede hacer pero la documentación NO lo recomienda.
-  const blackBears = userBearsStore((state) => state.blackBears);
-  const increaseBlackBears = userBearsStore((state) => state.increaseBlackBears);
-
   return (
     <>
       <h1>Contador de Osos</h1>
@@ -15,15 +9,7 @@ export const BearPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button onClick={() => increaseBlackBears(+1)}> +1 </button>
-            <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
-            <button onClick={() => increaseBlackBears(-1)}> -1 </button>
-          </div>
-        </WhiteCard>
+        <BlackBears />
 
         <WhiteCard centered>
           <h2>Osos Polares</h2>
@@ -46,5 +32,29 @@ export const BearPage = () => {
         </WhiteCard>
       </div>
     </>
+  );
+};
+
+export const BlackBears = () => {
+  // Refactorización:
+  // No solo por motivos de legibilidad. También hace que solo
+  // se renderice este componente en vez de todo el componente BearPage.
+
+  // Consumir el store.
+  // Notar que NO hacemos desestructuración.
+  // Se puede hacer pero la documentación NO lo recomienda.
+  const blackBears = userBearsStore((state) => state.blackBears);
+  const increaseBlackBears = userBearsStore((state) => state.increaseBlackBears);
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increaseBlackBears(+1)}> +1 </button>
+        <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
+        <button onClick={() => increaseBlackBears(-1)}> -1 </button>
+      </div>
+    </WhiteCard>
   );
 };
