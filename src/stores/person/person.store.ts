6e2612c53f19304e9f978c2cc9b87776107e3387
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-22 06:25:03 +0200

    2 - Middlewares - StateCreator Interface

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,4 +1,6 @@
-import { create } from 'zustand';
+// Se indica type para que cuando se construya la aplicación no importe nada, ya que
+// solo lo vamos a usar como una interface para TypeScript.
+import { type StateCreator, create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 // El state puede separse en piezas del state (propiedades y objetos) y métodos.
@@ -14,25 +16,24 @@ interface Actions {
   setLastName: (value: string) => void;
 }
 
+// Hemos sacado el state del interior de los middlewares para que sea más legible y mantenible.
+const storeApi: StateCreator<PersonState & Actions> = (set) => ({
+  firstName: '',
+  lastName: '',
+
+  // Como no estamos usando el state esto prodría quedar:
+  // setFirstName: (value: string) => set({ firstName: value }),
+
+  // Pero para explicar más fácilmente una cosa de los middlewares lo vamos a dejar así.
+  setFirstName: (value: string) => set((state) => ({ firstName: value })),
+  setLastName: (value: string) => set((state) => ({ lastName: value })),
+});
+
+// Esta parte queda ahora solo para puros middlewares.
 // Para usar un middleware envolveremos el (set) y nuestra configuración del store con la función
 // middleware que queramos usar.
 //
 // Persist Middleware: La idea es que cuando se pulse refrescar en el navegador no se pierda
 // la información. Se indica el nombre de la función persist y hay que añadir un objeto adicional con el
 // name, que será el nombre del storage que le quiero dar por defecto en el Local Storage.
-export const usePersonStore = create<PersonState & Actions>()(
-  persist(
-    (set) => ({
-      firstName: '',
-      lastName: '',
-
-      // Como no estamos usando el state esto prodría quedar:
-      // setFirstName: (value: string) => set({ firstName: value }),
-
-      // Pero para explicar más fácilmente una cosa de los middlewares lo vamos a dejar así.
-      setFirstName: (value: string) => set((state) => ({ firstName: value })),
-      setLastName: (value: string) => set((state) => ({ lastName: value })),
-    }),
-    { name: 'person-storage' }
-  )
-);
+export const usePersonStore = create<PersonState & Actions>()(persist(storeApi, { name: 'person-storage' }));
