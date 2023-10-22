fe0cf6d0db08353ae92e635af37eb099bdfbb951
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-22 06:35:15 +0200

    2 - Middlewares - Custom Storage - SessionStorage

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,7 +1,7 @@
 // Se indica type para que cuando se construya la aplicación no importe nada, ya que
 // solo lo vamos a usar como una interface para TypeScript.
 import { type StateCreator, create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { StateStorage, createJSONStorage, persist } from 'zustand/middleware';
 
 // El state puede separse en piezas del state (propiedades y objetos) y métodos.
 // Lo hacemos aquí, pero esto tiene mucho más sentido con los Zustand Slices
@@ -29,6 +29,22 @@ const storeApi: StateCreator<PersonState & Actions> = (set) => ({
   setLastName: (value: string) => set((state) => ({ lastName: value })),
 });
 
+// Este es nuestro objeto custom storage.
+// Lo acabaremos separando en un archivo independiente.
+const sessionStorage: StateStorage = {
+  // El name es el nombre del storage
+  getItem: function (name: string): string | Promise<string | null> | null {
+    console.log('getItem', name);
+    return null;
+  },
+  setItem: function (name: string, value: string): void | Promise<void> {
+    console.log('setItem', { name, value });
+  },
+  removeItem: function (name: string): void | Promise<void> {
+    console.log('removeItem', name);
+  },
+};
+
 // Esta parte queda ahora solo para puros middlewares.
 // Para usar un middleware envolveremos el (set) y nuestra configuración del store con la función
 // middleware que queramos usar.
@@ -36,4 +52,10 @@ const storeApi: StateCreator<PersonState & Actions> = (set) => ({
 // Persist Middleware: La idea es que cuando se pulse refrescar en el navegador no se pierda
 // la información. Se indica el nombre de la función persist y hay que añadir un objeto adicional con el
 // name, que será el nombre del storage que le quiero dar por defecto en el Local Storage.
-export const usePersonStore = create<PersonState & Actions>()(persist(storeApi, { name: 'person-storage' }));
+//
+// Usando nuestro Persist Middleware vamos crear un custom Storage para guardar nuestra información
+// en el Session Storage en vez del Local Storage.
+// Para ello asignaremos en la propiedad storage el objeto sessionStorage creado arriba.
+export const usePersonStore = create<PersonState & Actions>()(
+  persist(storeApi, { name: 'person-storage', storage: createJSONStorage(() => sessionStorage) })
+);
