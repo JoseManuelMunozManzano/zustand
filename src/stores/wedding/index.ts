260fd7169e9e9a3fcab609b089412105fd79812f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-27 18:46:57 +0200

    4 - Slices - Consumir Store en WeddingInvitationPage

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,13 +1,16 @@
 import { create } from 'zustand';
 import { type PersonSlice, createPersonSlice } from './person.slice';
+import { devtools } from 'zustand/middleware';
 
 type ShareState = PersonSlice;
 
 // El store es la combinación de todos los slices
 export const useWeddingBoundStore = create<ShareState>()(
-  // Une set, get y storeApi (...a) en un solo arreglo usando el operador rest
-  // para poderlo esparcir fácilmente usando el operador spread
-  (...a) => ({
-    ...createPersonSlice(...a),
-  })
+  devtools(
+    // Une set, get y storeApi (...a) en un solo arreglo usando el operador rest
+    // para poderlo esparcir fácilmente usando el operador spread
+    (...a) => ({
+      ...createPersonSlice(...a),
+    })
+  )
 );
