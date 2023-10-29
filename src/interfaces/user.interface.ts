039b8d2a3837ced7dd2402d5604e2292e7651d4d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-29 06:10:55 +0100

    5 - Peticiones HTTP - Conectar Login con Zustand

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -3,6 +3,6 @@ export interface User {
   id: string;
   email: string;
   fullName: string;
-  isActive: string;
+  isActive: boolean;
   roles: string[];
 }
--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -1,5 +1,8 @@
-import { StateCreator } from 'zustand';
+import { StateCreator, create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
 import type { AuthStatus, User } from '../../interfaces';
+import { AuthService } from '../../services/auth.service';
 
 // Como luce el state del store.
 // Como en un determinado punto en el tiempo no tendré el token ni el usuario, ambos son opcionales.
@@ -7,14 +10,29 @@ export interface AuthState {
   status: AuthStatus;
   token?: string;
   user?: User;
+
+  // No recibimos el parámetro Rememberme porque no lo vamos a usar.
+  loginUser: (email: string, password: string) => Promise<void>;
 }
 
 // Se indica el paréntesis antes que la llave porque es un return implícito de un objeto.
-export const storeApi: StateCreator<AuthState> = (set) => ({
+const storeApi: StateCreator<AuthState> = (set) => ({
   status: 'unauthorized',
 
   // Como son optativas, no haría falta definirlas, pero indicando undefined es más
   // fácil de leer.
   token: undefined,
   user: undefined,
+
+  // Aquí conectamos el servicio
+  loginUser: async (email: string, password: string) => {
+    try {
+      const { token, ...user } = await AuthService.login(email, password);
+      set({ status: 'authorized', token, user });
+    } catch (error) {
+      set({ status: 'unauthorized', token: undefined, user: undefined }); // Igual a un logout
+    }
+  },
 });
+
+export const useAuthStore = create<AuthState>()(devtools(persist(storeApi, { name: 'auth-storage' })));
